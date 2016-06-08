a54455a8bc34121dfbce5de6eb80533587b6b42f
codewc <1164004116@qq.com>
2016-06-08 18:24:49 +0800

    add
    Change-Id: Ia147a7701a90a846a4d4d249a9a385c1810ca6d7
    Signed-off-by: codewc <1164004116@qq.com>

---
+++ pydev_temp/com/wc/tools/test/test.py
@@ -0,0 +1,10 @@
+'''
+Created on 2016年6月8日
+
+@author: 王春
+'''
+import tools4txt
+
+
+if __name__ == '__main__':
+    tools4txt.MyClass
---
+++ pydev_temp/com/wc/tools/test/tools4txt.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+'''
+Created on 2016��6��8��
+
+@author: wangchun
+@since: 2016-06-08
+'''
+from builtins import open
+class TextTool(object):
+    '''
+    classdocs
+    '''
+    
+
+    def __init__(self, params):
+        '''
+        Constructor"
+        '''
+    def tool(self):
+        o  = open()
